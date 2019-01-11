123c91cb06aee97a7f8ba2eca594fe2821142052
TinyChief <narsacgm@gmail.com>
2019-01-11 21:42:10 +0300

    Markdown proccessing w/ remark

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,17 +1,13 @@
-// This is where project configuration and installed plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
 module.exports = {
   siteName: process.env.SITE_NAME || 'Dev personal',
   siteDescription: process.env.SITE_DESC || 'Personal site of web developer.',
-  // titleTemplate: process.env.SITE_TITLE || '%s - <siteName>',
 
   plugins: [
     {
-      use: '~/plugins',
+      use: '~/md',
       options: {
         version: 'published',
-        token: process.env.TOKEN || 'mv4E8eaPszE2EySWP7Cv6Qtt', // Storyblok token
+        token: process.env.TOKEN, // Storyblok token
         route: '/blog/:slug',
         typeName: 'Post',
         folder: 'blog'
