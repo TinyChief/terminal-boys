b0e0a245c9f298d296d6cf453d1b6d393abc76ea
TinyChief <narsacgm@gmail.com>
2019-02-12 23:50:32 +0300

    site meta, seo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue) {
+export default function (Vue, { head }) {
+  head.htmlAttrs = {
+    lang: 'en',
+    charset: 'utf-8'
+  }
+  head.title = 'Главная страница сайта Юлдашбаева Вадима'
+  head.meta.push({
+    name: 'keywords',
+    content: 'Blog,HTMLCSS,JavaScript,Developer,Web'
+  })
+  head.meta.push({
+    name: 'description',
+    content: 'Персональный сайт Вадима Юлдашбаева'
+  })
   Vue.component('Layout', DefaultLayout)
 }
